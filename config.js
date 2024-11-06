c419b95ac3ab7020b54525b7705492b536646ead
Raul Garcia <raul.garcia@iepcjalisco.org.mx>
2024-11-06 13:28:27 -0600

    se agrega sequelize migrations

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
+const sequelize = require('./config/db');
 
 module.exports = {
     PORT: process.env.PORT,
     DB_URL: process.env.DB_URL,
-    JWT_SECRET: process.env.JWT_SECRET
+    JWT_SECRET: process.env.JWT_SECRET,
+    development: {
+        use_env_variable: 'DB_URL',
+        dialect: 'mysql', 
+    }
 };
--- config/db.js
+++ config/db.js
@@ -3,10 +3,18 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_URL , {
     logging: false, // Desactiva el registro de consultas SQL
+    dialect: 'mysql',
     define: {
         timestamps: true  // Si estás utilizando timestamps
       },
       timezone: '-06:00' 
 });
+sequelize.sync({ force: false })  // Cambia a `force: true` solo si deseas que las tablas existentes se reemplacen.
+    .then(() => {
+        console.log('Tablas sincronizadas correctamente');
+    })
+    .catch(error => {
+        console.error('Error al sincronizar las tablas:', error);
+    });
 
 module.exports = sequelize;
